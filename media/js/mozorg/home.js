2d2977b31b4e5857429b65209e1122a764a3e275
Isac Lagerblad <icaaaq@gmail.com>
2013-05-02 19:28:10 +0200

    Revert "removed unused objects"
    This reverts commit a36bf306aac93ff1b58676e3ed13ae046f9a913c.

--- media/js/mozorg/home.js
+++ media/js/mozorg/home.js
@@ -1,3 +1,16 @@
+if (typeof Mozilla === 'undefined') {
+    var Mozilla = {};
+}
+
+if (typeof Mozilla.page === 'undefined') {
+    Mozilla.page = {};
+}
+
+Mozilla.page.Home = {
+    'closeText' : $('#strings').data('close'),
+    'shareText' : $('#strings').data('share')
+};
+
 $(document).ready(function() {
 
     // {{{ showLink()
@@ -61,7 +74,7 @@ $(document).ready(function() {
                     if (!$shareButton) {
                         $shareButton = $(
                             '<button class="vjs-sandstone-share">' +
-                                $('#strings').data('share') +
+                            Mozilla.page.Home.shareText +
                             '</button>'
                         );
                         $shareButton.click(function() {
@@ -431,7 +444,7 @@ $(document).ready(function() {
             '<span class="video-close" tabindex="0" role="button">×</span>'
         );
 
-        $close.attr('title', $('#strings').data('close'))
+        $close.attr('title', Mozilla.page.Home.closeText)
             .click(function(e) { close(); })
             .keypress(function(e) {
                 if (e.keyCode === 13 || e.keyCode === 32) {
